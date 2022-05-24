074d279e3031088a1d174cd5e399c9858ed9ea77
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-05-24 12:06:18 -0400

    build: add vite and related dependencies

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    // Relative to the root
+    outDir: "../build",
+  },
+  plugins: [
+    // …
+    react({
+      // Use React plugin in all *.jsx and *.tsx files
+      include: "**/*.{jsx,tsx}",
+    }),
+  ],
+});
